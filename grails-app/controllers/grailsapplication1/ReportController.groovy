874b368b94a029855eca1f210865cb8f348bb472
ziaziek <ziaziek@poczta.fm>
2016-12-15 10:39:20 +0100

    Report controller Work In Progress

--- grails-app/controllers/grailsapplication1/ReportController.groovy
+++ grails-app/controllers/grailsapplication1/ReportController.groovy
@@ -5,7 +5,11 @@ class ReportController {
     def index() { }
     
     def expiredBooks() {
-        
+        def expiredBooksIds = []
+        Metrics.findAllByExpiryDateLessThan(new Date()).each {it-> expiredBooksIds << it.book.id} 
+        System.out.println(expiredBooksIds.size())
+        def expiredBooks = Book.getAll(expiredBooksIds)
+        [books: expiredBooks, count: expiredBooks==null ? 0 : expiredBooks.size()]
     }
     
     def expiredHoldingBorrowers(){
